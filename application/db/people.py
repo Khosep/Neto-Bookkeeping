ee588c5bdc3e4a1454b32f3419c8f2a3e213f2eb
Khosep <khose007@yandex.ru>
2022-07-01 23:30:13 +0500

    First commit: Add files

---
+++ application/db/people.py
@@ -0,0 +1,3 @@
+import datetime
+def get_employees():
+    print(f'<{datetime.date.today()}: Getting employees>')
---
+++ application/salary.py
@@ -0,0 +1,3 @@
+import datetime
+def calculate_salary():
+    print(f'<{datetime.datetime.now().date()}: Calculating salary>')
